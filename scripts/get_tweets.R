3f1f41e6699409896c919e0cb2bc0a203c02b1ea
jsta <stachel2@msu.edu>
2021-02-15 14:38:53 -0500

    create token

--- scripts/get_tweets.R
+++ scripts/get_tweets.R
@@ -1,6 +1,13 @@
 # ---- setup ----
 library(rtweet)
 
+dashboard_token <- rtweet::create_token(
+  consumer_key =    Sys.getenv("TWITTER_CONSUMER_API_KEY"),
+  consumer_secret = Sys.getenv("TWITTER_CONSUMER_API_SECRET"),
+  access_token =    Sys.getenv("TWITTER_ACCESS_TOKEN"),
+  access_secret =   Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+)
+
 read_latest <- function(){
   archives <- list.files("data", pattern = "*likes.rds",
                          full.names = TRUE, include.dirs = TRUE)
