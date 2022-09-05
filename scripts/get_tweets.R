d774739660995172401673f85ad42e3d6a806cf9
jsta <stachel2@msu.edu>
2022-09-05 07:12:57 -0600

    update auth attempt

--- scripts/get_tweets.R
+++ scripts/get_tweets.R
@@ -4,9 +4,9 @@ library(rtweet)
 
 
 if(!is.null(grep("comp", Sys.info()["nodename"]))){ # not on jsta local system
-  dashboard_token <- rtweet::create_token(
-    consumer_key =    Sys.getenv("TWITTER_CONSUMER_API_KEY"),
-    consumer_secret = Sys.getenv("TWITTER_CONSUMER_API_SECRET"),
+  dashboard_token <- rtweet::rtweet_bot(
+    api_key =    Sys.getenv("TWITTER_CONSUMER_API_KEY"),
+    api_secret = Sys.getenv("TWITTER_CONSUMER_API_SECRET"),
     access_token =    Sys.getenv("TWITTER_ACCESS_TOKEN"),
     access_secret =   Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET")
   )
